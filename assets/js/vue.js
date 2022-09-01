58495a980039e0d4b5fc9af3d6b23bbe28ee605f
s757129 <2858727468@qq.com>
2022-09-01 12:12:46 +0800

    v2.0

---
+++ assets/js/vue.js
@@ -0,0 +1,90 @@
+Vue.config.productionTip = false
+const vm = new Vue({
+    data() {
+        return {
+            name: '暮光壁纸',
+            logo: './assets/img/logo.png',
+            version: '2.4.26',
+            volume: '8.77MB',
+            time: '2022-08-26',
+            introduction: '永久免费无广告，高清壁纸软件，素材一应俱全',
+            download: [
+                {
+                    name: '立即下载',
+                    url: 'https://www.123pan.com/s/ikkrVv-cP5Qh'
+                },
+                {
+                    name: '备用下载',
+                    url: './old'
+                }
+            ],
+            screenshot: [
+                {
+                    image: './assets/img/1.png'
+                },
+                {
+                    image: './assets/img/2.png'
+                },
+                {
+                    image: './assets/img/3.png'
+                },
+                {
+                    image: './assets/img/4.png'
+                },
+                {
+                    image: './assets/img/5.png'
+                },
+                {
+                    image: './assets/img/6.png'
+                },
+                {
+                    image: './assets/img/7.png'
+                },
+            ],
+            update: [
+                {
+                    log: '1. 优化应用功能布局，优化架构、响应速度'
+                },
+                { 
+                    log: '2. 修改了壁纸预览、头像预览的界面' 
+                },
+                { 
+                    log: '3. 各搜索壁纸头像功能整合成为聚合搜图' 
+                },
+                { 
+                    log: '4. 壁纸大全聚合了不同的10个接口' 
+                },
+                { 
+                    log: '5. 新增哔哩哔哩解析功能' 
+                },
+                { 
+                    log: '6. 各界面统一布局，界面更加美观' 
+                },
+                {
+                    log: '7. 已将MiSans删减版字体改用Material Design字体'
+                }
+            ],
+            history: [
+                {
+                    version: '2.3.25',
+                    volume: '12.45MB',
+                    time: '2022-05-25',
+                    url: 'https://wwa.lanzoui.com/itWwd05exy7e'
+                },
+                {
+                    version: '2.2.24',
+                    volume: '15.78MB',
+                    time: '2022-02-24',
+                    url: '/'
+                }
+            ],
+            friend:[
+                {
+                    name:'柒伍七の鱼',
+                    url:'https://s757129.github.io'
+                }
+            ]
+        }
+    }
+})
+vm.$mount('#root')
---
+++ old/assets/js/vue.js
@@ -0,0 +1,50 @@
+Vue.config.productionTip = false
+const vm = new Vue({
+    data() {
+        return {
+            name: '暮光壁纸',
+            logo: './assets/img/logo.png',
+            version: '2.4.26',
+            downloads: [
+                {
+                    name: '蓝奏云',
+                    url: 'https://wwa.lanzoui.com/itWwd05exy7e'
+                },
+                {
+                    name: '123网盘',
+                    url: 'https://www.123pan.com/s/ikkrVv-cP5Qh'
+                }
+            ],
+            contact: [
+                {
+                    icon: 'ri-home-smile-line',
+                    name: '体验新版',
+                    url: '/'
+                },
+                {
+                    icon: 'ri-user-smile-line',
+                    name: '商务合作',
+                    url: 'https://space.bilibili.com/476467975'
+                }
+            ],
+            friends: [
+                {
+                    name: '柒伍七の鱼',
+                    url: 'https://s757129.github.io',
+                    gap: '|'
+                },
+                {
+                    name: '油猴中文网',
+                    url: 'https://bbs.tampermonkey.net.cn/?fromuid=27974',
+                    gap: '|'
+                },
+                {
+                    name: '脚本猫',
+                    url: 'https://scriptcat.org/search',
+                    gap: ''
+                }
+            ]
+        }
+    }
+})
+vm.$mount('#root')
